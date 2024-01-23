d1f382a188c8941357af72e48c63d7dcf9307f63
karthik06kn <68649988+karthik06kn@users.noreply.github.com>
2024-01-23 11:46:03 +0530

    added backend - graphQL

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappc3cba9d7": {
       "AppClientID": "string",
